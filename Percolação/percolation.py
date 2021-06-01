4b8871e90607de4d7f80fae2cca396e88dd11ec8
juliapocciotti <juliapocciotti@gmail.com>
2021-06-01 18:37:25 -0300

    add percolation updated

--- Percolação/percolation.py
+++ Percolação/percolation.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 
 #-------------------------------------------------------------------------- 
@@ -15,32 +14,47 @@ class Percolation:
         if type(shape) == int:
             self.shape = shape, shape
             self.grid = np.full(self.shape, BLOCKED)
+
         elif type(shape) == tuple:
             self.grid = np.full(shape, BLOCKED)
             self.shape = shape
-      
-            
+                 
     def __str__(self):
+        
         n, m = self.shape
         rede = ''
+        
         for i in range(n):
             rede += '+'
             for x in range(m):
                 rede += '---+'
             
             rede += '\n'
-            rede += '|   ' * (m+1) 
+            for x in range(m):
+                if self.grid[i, x] == BLOCKED:
+                    rede += '|   ' 
+                elif self.grid[i, x] == OPEN:
+                    rede += '| o '
+                elif self.grid[i, x] == FULL:
+                    rede += '| x '
+            
+            rede += '|'
             rede += '\n'
         
         final = '+' + '---+' * (m) 
         rede += final 
         
         rede += f'\ngrade de dimensão: {n}x{m}'
+        rede += f'\nno. sítios abertos: {self.no_open()}'
+        rede += f'\npercolou: {self.percolates()}'
         return rede 
 
     def is_open(self, lin, col):
-        if self.grid[lin, col] == OPEN:
+        if self.grid[lin, col] == FULL:
             return True
+        elif self.grid[lin, col] == OPEN:
+            return True
+        
         return False 
             
     def is_full(self, lin, col):
@@ -52,15 +66,105 @@ class Percolation:
         n_open = []
         for i in self.grid[:, :]:
             for j in i:
-                if j == OPEN:
+                if j == OPEN or j == FULL:
                     n_open.append(j)
         return len(n_open)
     
     def get_grid(self):
-        return self.grid[:]
+        return self.grid.copy()
+    
+    def open(self, lin, col):        
+
+        if self.grid[lin,col] == BLOCKED:
+           self.grid[lin, col] = OPEN
+    
+        if lin == 0:
+            self.grid[lin, col] = FULL
+            return None 
         
+        visitas = np.full(self.shape, 0)
         
+        q = Fila()
         
+        n, m  = self.shape
         
+        q.insere((lin, col))
+        repetidor = 0 
+        while not q.vazia():
+            vizinhos = [(lin-1, col),
+                        (lin+1, col),
+                        (lin, col+1),
+                        (lin, col-1)]
+            
+            for v in range(len(vizinhos)):
+                linha, coluna = vizinhos[v]
+                
+                if  0 <= linha < n and  0 <= coluna < m:
+                
+                    if self.grid[linha, coluna] != BLOCKED:
+                
+                        if visitas[linha,coluna] != 1:
+                            q.insere((linha,coluna))
+                            visitas[linha,coluna] = 1
+            
+            i, j = q.remove()
+            if self.grid[i, j] == FULL:
+                self.grid[lin, col] = FULL
+            
+            if self.grid[i, j] == OPEN and self.grid[lin, col] == OPEN:
+                if repetidor < 2:
+                    q.insere((i,j))
+                    repetidor += 1
+            
+            while self.grid[i,j] == OPEN and self.grid[lin,col] == FULL:
+                
+                self.grid[i, j] = FULL  
+                lin, col = i, j
+                vizinhos = [(lin-1, col),
+                            (lin+1, col),
+                            (lin, col+1),
+                            (lin, col-1)]
+
+                for v in range(len(vizinhos)):
+                    linha, coluna = vizinhos[v]        
+                    
+                    if  0 <= linha < n and  0 <= coluna < m:
+                    
+                        if self.grid[linha, coluna] != BLOCKED:
+                            
+                            q.insere((linha,coluna))
+                            visitas[linha,coluna] = 1
+                
+                
+    def percolates(self):
+        full = []
+       
+        n = len(self.grid)-1
+        for i in self.grid[n]:
+            if i == FULL:
+                full.append(i)              
+
+        if len(full)>=1:
+            return True
         
+        return False 
+
+class Fila:
+    def __init__(self):
+        self.itens = []
+
+    def __str__(self):
+        return str(self.itens)
         
+    def vazia(self):
+        return self.itens == []
+
+    def insere(self, item):
+        self.itens.append(item) 
+
+    def remove(self):
+        return self.itens.pop(0) 
+
+    def __len__(self):
+        return len(self.itens)
+           
